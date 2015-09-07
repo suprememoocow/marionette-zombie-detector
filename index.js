85d09b13203c16c992c7d45762c71a6d9cb4eec2
Andrew Newdigate <andrew@troupe.co>
2015-09-08 00:30:29 +0100

    Track all EventListenerTarget event listener adds and removals

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-'use strict';
-
 function isAttachedToDocument(el) {
   for(var parentElement = el.parentElement; parentElement; parentElement = parentElement.parentElement) {
     if (parentElement === document.documentElement) return true;
   }
 
   return false;
+}
+
+function findZombieEvents(view) {
+  var nodeIterator = document.createNodeIterator(view.el, NodeFilter.SHOW_ELEMENT);
+
+  if (view.el._debugEventListeners && view.el._debugEventListeners.length) {
+    console.log('Zombie listeners detected', view, view.el._debugEventListeners);
+  }
+
+  var currentNode;
+  while ((currentNode = nodeIterator.nextNode())) {
+    if (currentNode._debugEventListeners && currentNode._debugEventListeners.length) {
+      console.log('Zombie listeners detected', view, currentNode._debugEventListeners);
+    }
+  }
 }
 
 function onRemoved() {
@@ -20,12 +33,51 @@ function onRemoved() {
 
     if (!view.isDestroyed) {
       console.log('Zombie detected', view);
+    } else {
+      findZombieEvents(view);
     }
   });
 }
 
+function indexOfListener(el, type, fn, capture) {
+  if (!el._debugEventListeners) return -1;
+  for (var i = 0; i < el._debugEventListeners.length; i++) {
+      var item = el._debugEventListeners[i];
+      if (item.type === type && item.fn === fn && item.capture === capture) {
+        return i;
+      }
+  }
+  return -1;
+}
+
+function installEventTargetListeners() {
+  var addEventListener = EventTarget.prototype.addEventListener;
+  EventTarget.prototype.addEventListener = function(type, fn, capture) {
+    var i = indexOfListener(this, type, fn, capture);
+    if (i === -1) {
+      if (!this._debugEventListeners) {
+      	this._debugEventListeners = [];
+    	}
+	    this._debugEventListeners.push({ type: type, fn: fn, capture: capture});
+    }
+
+    return addEventListener.apply(this, arguments);
+  }
+
+  var removeEventListener = EventTarget.prototype.removeEventListener;
+  EventTarget.prototype.removeEventListener = function(type, fn, capture) {
+    var i = indexOfListener(this, type, fn, capture);
+    if (i >= 0) {
+      this._debugEventListeners.splice(i, 1);
+    }
+    return removeEventListener.apply(this, arguments);
+  }
+}
+
 module.exports = {
   install: function(Marionette) {
+    installEventTargetListeners();
+
     var ViewConstructor = Marionette.View;
     var ViewPrototype = Marionette.View.prototype;
     Marionette.View = function() {
